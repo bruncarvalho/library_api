444b2961729a3110d34ec38fa5e078d4e1da9999
Bruna Dias <bruna.dias@maxmilhas.com.br>
2026-07-08 20:58:39 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,15 @@
+### Biblioteca
+
+## Objetivo
+Está api trata-se de uma biblioteca de livros
+Linguagem JavaScript usando API REST
+
+## Requisitos
+
+- Node.js 22+
+- Instale as dependências: `npm install`.
+
+## Ambiente development com Docker Compose
+
+O repositório possui um `docker-compose.yml` na raiz
+Inicie os serviços: `docker compose up -d`
---
+++ database/init.sql
@@ -0,0 +1,18 @@
+CREATE TABLE IF NOT EXISTS authors (
+  id INT NOT NULL AUTO_INCREMENT,
+  name VARCHAR(100) NOT NULL UNIQUE,
+  stage_name VARCHAR (100),
+  birth_year DATE NOT NULL,
+  date_of_death DATE,
+  PRIMARY KEY (id)
+);
+
+CREATE TABLE IF NOT EXISTS books (
+  id INT NOT NULL AUTO_INCREMENT,
+  author_id INT NOT NULL,
+  title VARCHAR(100) NOT NULL,
+  year_of_publication DATE,
+  CONSTRAINT fk_books_author FOREIGN KEY (author_id) REFERENCES authors (id),
+  PRIMARY KEY (id)
+);
+
