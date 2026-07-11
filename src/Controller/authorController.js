9d3d94d5032f43e523900c190699b43e3f65f966
Bruna Dias <bruna.dias@maxmilhas.com.br>
2026-07-10 23:00:59 -0300

    controller start

--- src/Controller/authorController.js
+++ src/Controller/authorController.js
@@ -1,2 +1,9 @@
-import { authors } from '../routes/authors.js'
+import { Router } from 'express';
 
+const router = Router()
+
+router.post('/', (req, res) => {
+  res.send('Ok')
+});
+
+export default router
--- src/index.js
+++ src/index.js
@@ -1,8 +1,15 @@
 import express from 'express';
+import  authorController  from './Controller/authorController.js';
 
 const app = express();
 
 let PORT = process.env.PORT || 3000
+
+app.post('/', (req, res) => {
+  res.send('inicio')
+})
+
+app.use('/author', authorController)
 
 app.listen(PORT, () => {
   console.log(`Servidor rodando em http://localhost:${PORT}`)
